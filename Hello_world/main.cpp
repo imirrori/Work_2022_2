1774ff8b95a1b1f887cb36c17537561c7d8e9946
Evgeniy Kozev <legionsnov@rambler.ru>
2022-08-20 13:52:01 +0300

    Добавил новую строку

--- Hello_world/main.cpp
+++ Hello_world/main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "вфалдонпрвфылшопрло" << endl;
     return 0;
 }
