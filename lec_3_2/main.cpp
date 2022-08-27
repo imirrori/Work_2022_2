3124710db9a8ebb5bc78e1b6710054f6112fe491
Evgeniy Kozev <legionsnov@rambler.ru>
2022-08-27 13:14:55 +0300

    Программа с менюшкой

---
+++ lec_3_2/main.cpp
@@ -0,0 +1,38 @@
+﻿// Написать программу которая имеет меню с выбором математической функции
+
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    int ch = 0;
+    do
+    {
+        switch(ch)
+        {
+        case 1:
+            {
+                double x;
+                printf("Please enter X: ");
+                scanf("%lf", &x);
+                printf("cos(X) : %f\n", cos(x));
+                break;
+            }
+        case 2:
+            {
+                double x;
+                printf("Please enter X: ");
+                scanf("%lf", &x);
+                printf("sin(X) : %f\n", sin(x));
+                break;
+            }
+        }
+
+        printf("1. cos()\n");
+        printf("2. sin()\n");
+        printf("3. exit\n");
+        scanf("%d", &ch);
+
+    } while(ch != 3);
+    return 0;
+}
