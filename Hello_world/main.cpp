eaba0765237b87b5445e3dcb5ce22ca4c1ef2278
Evgeniy Kozev <legionsnov@rambler.ru>
2022-08-27 11:40:21 +0300

    Какие изменения

--- Hello_world/main.cpp
+++ Hello_world/main.cpp
@@ -1,10 +1,14 @@
-﻿#include <iostream>
-
-using namespace std;
+﻿#include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout << "вфалдонпрвфылшопрло" << endl;
+    const int a = 42;
+    const int b = 43;
+    const int c = a == b ? 12 && 10 : 2 - 4;
+
+    printf("Max value %d", c);
+
     return 0;
+
 }
