67f012c8bc151c220255ef89a900974546912d05
Evgeniy Kozev <legionsnov@rambler.ru>
2022-08-27 13:08:25 +0300

    Деление без остатка

--- Hello_world/main.cpp
+++ Hello_world/main.cpp
@@ -1,14 +1,11 @@
-﻿#include <stdio.h>
-#include <math.h>
+﻿// НАписать программу получающую на
+
+#include <stdio.h>
 
 int main()
 {
-    const int a = 42;
-    const int b = 43;
-    const int c = a == b ? 12 && 10 : 2 - 4;
-
-    printf("Max value %d", c);
-
-    return 0;
 
 }
+
+
+
---
+++ lec_3_1/main.cpp
@@ -0,0 +1,20 @@
+﻿//Написать программу, получающую натуральное число N
+//Вывести все натуральные числа, на которые делится N, без остатка
+
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    printf("Please enter N: ");
+    scanf("%d", &N);
+    printf("Divider N: ");
+    for(int i = 1; i < N; ++i)
+    {
+        if((N % i) == 0)
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
