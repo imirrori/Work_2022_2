dccfad2cf06c1f189a2ce62bc18b78a1ebb6b134
Evgeniy Kozev <legionsnov@rambler.ru>
2022-09-10 11:46:08 +0300

    drfhbxf

---
+++ LEC_5_1/main.cpp
@@ -0,0 +1,29 @@
+﻿// Дана строка символов. Определить, является ли она правильным скобочным выражением.
+// () - да
+// (()) - да
+// )( - нет
+
+#include <stdio.h>
+
+int main()
+{
+    const char* str_1 = "()";
+    int count = 0;
+
+    while(*str_1) {
+        if (*str_1 == '(') {
+            ++count;
+        } else if (*str_1 == ')') {
+            --count;
+            if (count < 0) {
+                printf("false\n");
+                return 0;
+            }
+        }
+        ++str_1;
+    }
+
+    printf("%s\n", count == 0 ? "true" : "false");
+
+    return 0;
+}
---
+++ LEC_5_2/main.cpp
@@ -0,0 +1,25 @@
+﻿// Дана строка символов. Удалить из нее все слова нечетной длины.
+// Слова отделяются друг от друга одним пробелом.
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[255];
+
+    printf("Please enter string: ");
+    gets(str);
+
+    char word[255];
+    int len = 0;
+    while(sscanf(str + len, "%s", word) == 1) {
+        const int wordLen = strlen(word);
+        len += wordLen + 1;
+        if ((wordLen % 2) == 1) {
+            printf("%s\n", word);
+        }
+    }
+
+    return 0;
+}
